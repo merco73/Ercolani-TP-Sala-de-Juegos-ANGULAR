b1b920e6184a924cd2a2c04361b64f4c12b0c60c
matiasercolani <matiaserc514@gmail.com>
2021-03-26 18:51:51 -0300

    Ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component: HomeComponent },
+  { path:'**', pathMatch:'full', redirectTo:'home' },
+  { path:'login', component: LoginComponent },
+  { path:'quien-soy', component: QuienSoyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
