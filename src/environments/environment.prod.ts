46d135f521bc1f997cb343b7a534bfb010099c48
matiasercolani <matiaserc514@gmail.com>
2021-04-15 17:34:09 -0300

    prueba 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,16 @@
+// export const environment = {
+//   production: true
+// };
+
 export const environment = {
-  // production: true
-  production: false
+  // production: false,
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyBIpj7ZkI9GrZT6WC_-RPX04gFzVoYJvaU",
+  authDomain: "tpsaladejuegos-86390.firebaseapp.com",
+  projectId: "tpsaladejuegos-86390",
+  storageBucket: "tpsaladejuegos-86390.appspot.com",
+  messagingSenderId: "145775191809",
+  appId: "1:145775191809:web:f350b3bc59b5dc25e95d00"
+  }
 };
